2460721847407ec6adab0caeae0f0c8414f8ef96
Jona <jonic.ctba@gmail.com>
2026-08-16 19:33:40 +0200

    Make all document tool rows full width

--- src/app/tools/layout.tsx
+++ src/app/tools/layout.tsx
@@ -5,8 +5,12 @@ export default function ToolsLayout({ children }: { children: React.ReactNode })
   return (
     <>
       <div className="[&_.grid]:grid-cols-1">{children}</div>
-      <PdfExtraTools />
-      <NewPublicTools />
+      <div className="w-full [&>section]:!w-full [&>section]:!max-w-none [&>section]:px-4 sm:[&>section]:px-8 [&_section]:!w-full">
+        <PdfExtraTools />
+      </div>
+      <div className="w-full [&>section]:!w-full [&>section]:!max-w-none [&>section]:px-4 sm:[&>section]:px-8 [&_section]:!w-full">
+        <NewPublicTools />
+      </div>
     </>
   );
 }
