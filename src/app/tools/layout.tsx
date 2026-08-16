42ab54a396a157b0ab7f2ae84d86159a8568328c
Jona <jonic.ctba@gmail.com>
2026-08-16 19:24:10 +0200

    Add new public tools to document tools list

--- src/app/tools/layout.tsx
+++ src/app/tools/layout.tsx
@@ -1,10 +1,12 @@
 import PdfExtraTools from "@/components/document-tools/PdfExtraTools";
+import NewPublicTools from "@/components/document-tools/NewPublicTools";
 
 export default function ToolsLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <div className="[&_.grid]:grid-cols-1">{children}</div>
       <PdfExtraTools />
+      <NewPublicTools />
     </>
   );
 }
